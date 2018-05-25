26ddc80b7acba5bb333d59d74e97e7b938227372
Avelino Tiu III <thirdtiu@gmail.com>
2018-05-25 13:22:46 +0800

    generate tokens api

--- index.js
+++ index.js
@@ -101,5 +101,6 @@ const unifiedServer = (req, res) => {
 // define a request router
 const router = {
     'ping' : handlers.ping,
-    'users' : handlers.users
+    'users' : handlers.users,
+    'tokens' : handlers.tokens
 }
